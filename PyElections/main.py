b1544ad0cb24361832ec85235fa5c7402e240d27
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-02-21 22:00:16 -0600

    Added comments to existing script files

--- PyElections/main.py
+++ PyElections/main.py
@@ -1,11 +1,14 @@
+# Abdullah Almasri - PyElections challenge 
+
 import os
 import csv 
 import numpy as np
 
+#Importing data from the csv file as well as defining export path 
 csvpath = os.path.join("..", "PyElections", "election_data.csv")
 exportresults = os.path.join("..", "PyElections", "Election Results")
 
-
+#creating empy matrices to fill later 
 All_Candidates = []
 County_total = []
 Voter_ID = []
@@ -13,24 +16,27 @@ Voter_ID = []
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+#Splitting the rows into different matrices 
     for row in csvreader:
         All_Candidates.append(row[0])
         County_total.append(row[1])
         Voter_ID.append(row[2])
         
-
+#Finding the unique candiadates in the list of all candidates 
 Unique_Candidates = []
 for x in All_Candidates:
     if x not in Unique_Candidates:
         Unique_Candidates.append(x)
 
+#Finding the total count for each candidate as well as the percentage of vote they recieved. 
 Ind_count =[]
 perc_per =[]
 Total_Votes = len(All_Candidates)
 for i in range(len(Unique_Candidates)):
     Ind_count.append(All_Candidates.count(Unique_Candidates[i]))
     perc_per.append(round((Ind_count[i]/len(All_Candidates))*100,2))
-    
+
+#Sorting the data so that can it can come out properly as I want it
 perc_per, Unique_Candidates = zip(*sorted(zip(perc_per, Unique_Candidates)))
 perc_per, Unique_Candidates = (list(t) for t in zip(*sorted(zip(perc_per, Unique_Candidates))))
 Unique_Candidates.append(Unique_Candidates.reverse())
@@ -38,10 +44,12 @@ perc_per.append(perc_per.reverse())
 Ind_count.sort()
 Ind_count.append(Ind_count.reverse())
 
+#Finding the leading candidate and the second leading candidate 
 Firstcandidate = Unique_Candidates[0]
 Secondcandidate = Unique_Candidates[1]
 
 
+#Defining and printing the strings I want to print 
 Election_summary1 = ( 
     "Houston Mayoral Election Results \n"
     "---------------------------------------\n"
@@ -63,6 +71,7 @@ for i in range(len(Unique_Candidates)-1):
     
 print(Election_summary3)
 
+#Exporting the results into a text file. 
 with open(exportresults, "w") as txt_file:    
         txt_file.write(Election_summary1)
         for i in range(len(Unique_Candidates)-1):
--- PyFinances/main.py
+++ PyFinances/main.py
@@ -1,13 +1,18 @@
+#Abdullah Almasri - PyFinances Challenge
+
 import os
 import csv 
 
+#Importing data from the csv file 
 csvpath = os.path.join("..", "PyFinances", "budget_data.csv")
 exportresults = os.path.join("..", "PyFinances", "Financial Analysis")
+
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    Num_Months=0
+#seperating columns of csv file into different variables as well as counting the number of rows 
+    Num_Months=0      
     Profit_list= []
     Months = []
     for row in csvreader:
@@ -15,16 +20,19 @@ with open(csvpath, newline='') as csvfile:
         Profit_list.append(int(row[0]))
         Months.append(row[1])
 
+#Finding the profit sum using numpy 
 import numpy 
 Profit_sum = numpy.sum(Profit_list)
 
+#Finding the change in profits between consecutive months 
 Profit_change = []
-
 for i in range(len(Profit_list)-1):
     Profit_change.append(Profit_list[i+1]-Profit_list[i])
 
+#Average of the profit changes we found before 
 Average_Change = sum(Profit_change)/(len(Profit_change))
 
+#Max and min of the profit change and their respected months 
 max_change = max(Profit_change)
 min_change = min(Profit_change)
 
@@ -34,7 +42,9 @@ Max_month = Months[max_change_month]
 min_change_month = Profit_change.index(min_change)+1
 Min_month = Months[min_change_month]
 
+#Printing out the results from the above code in an organized fashion 
 Summary = (
+    "\n"
     "Financial Analysis \n"
     f"Total Months: {Num_Months}\n"
     f"Total: ${Profit_sum}\n"
@@ -43,6 +53,7 @@ Summary = (
     f"Greatest Decrease in Profits:{Min_month} (${min_change})\n")
 print(Summary)
 
+#Exporting the results to a txt file 
 with open(exportresults, "w") as txt_file:    
     txt_file.write(Summary)
 
